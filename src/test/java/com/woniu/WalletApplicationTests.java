5652e2e8c1cd965536b3a822c06da8a950a969cb
tjx <443016895@qq.com>
2019-08-19 20:14:34 +0800

    user表增查

--- src/test/java/com/woniu/WalletApplicationTests.java
+++ src/test/java/com/woniu/WalletApplicationTests.java
@@ -1,16 +1,29 @@
 package com.woniu;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.woniu.domain.User;
+import com.woniu.service.IUserService;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WalletApplicationTests {
-
+	@Resource
+	private IUserService service;
+	
 	@Test
-	public void contextLoads() {
+	public void test1() {
+		List<User> list = service.findAll();
+		for (User u : list) {
+			System.out.println(u);
+		}
 	}
 
 }
