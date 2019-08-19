0ce9a711f39187f4a212d018431b19784bced1b7
sy007 <sshenyuan@163.com>
2019-08-19 20:08:51 +0800

    查询 datetime

--- src/main/java/com/woniu/service/IDebitService.java
+++ src/main/java/com/woniu/service/IDebitService.java
@@ -2,6 +2,8 @@ package com.woniu.service;
 
 import java.util.List;
 
+import com.woniu.domain.Loantime;
+
 public interface IDebitService {
-	List<Integer> findAllLoantime();
+	List<Loantime> findAllLoantime();
 }
--- src/test/java/com/woniu/WalletApplicationTests.java
+++ src/test/java/com/woniu/WalletApplicationTests.java
@@ -1,16 +1,35 @@
 package com.woniu;
 
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.woniu.domain.Loantime;
+import com.woniu.service.IDebitService;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WalletApplicationTests {
 
+	@Resource
+	private IDebitService debitServiceImpl;
+	
 	@Test
 	public void contextLoads() {
 	}
-
+	
+	@Test
+	public void testName() throws Exception {
+		List<Loantime> list = debitServiceImpl.findAllLoantime();
+		for (Loantime loantime : list) {
+			System.out.println(loantime.getLoantime());
+		}
+	}
 }
