6b684c1d66c744d31b470e27b466018e65ff27b6
jack <1358558863@qq.com>
2019-08-21 16:06:01 +0800

    通过loandisplay嵌套查询到loanaplly 通过loanapply嵌套查询到loanrate

--- src/main/java/com/woniu/controller/InvestController.java
+++ src/main/java/com/woniu/controller/InvestController.java
@@ -1,20 +1,28 @@
 package com.woniu.controller;
 
 import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.woniu.domain.Loanapply;
 import com.woniu.domain.Loandisplay;
 import com.woniu.service.IInvestService;
 
 @RestController
 @RequestMapping("/invest/")
 public class InvestController {
+	@Resource
 	private IInvestService investServiceImpl;
 	
 	@RequestMapping("findAllLoanDisplay")
 	public List<Loandisplay> findAllLoanDisplay() {
+		List<Loandisplay> list = investServiceImpl.findAllLoadDisplay();
+		Loandisplay apply=list.get(0);
+		System.out.println(apply+" "+apply.getLoanapply().getLoanrate().getLoanrate());
 		return investServiceImpl.findAllLoadDisplay();
 	}
 
--- src/main/java/com/woniu/service/IInvestService.java
+++ src/main/java/com/woniu/service/IInvestService.java
@@ -1,6 +1,7 @@
 package com.woniu.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.woniu.domain.Loandisplay;
 
