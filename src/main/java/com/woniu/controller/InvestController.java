9c70ab9530c60810d99a821be04af0a3686e2aee
jack <1358558863@qq.com>
2019-08-21 20:53:54 +0800

    增加新方法

--- src/main/java/com/woniu/controller/InvestController.java
+++ src/main/java/com/woniu/controller/InvestController.java
@@ -5,8 +5,10 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.woniu.domain.Loanapply;
 import com.woniu.domain.Loandisplay;
@@ -22,8 +24,21 @@ public class InvestController {
 	public List<Loandisplay> findAllLoanDisplay() {
 		List<Loandisplay> list = investServiceImpl.findAllLoadDisplay();
 		Loandisplay apply=list.get(0);
-		System.out.println(apply+" "+apply.getLoanapply().getLoanrate().getLoanrate());
 		return investServiceImpl.findAllLoadDisplay();
+	}
+	
+	@RequestMapping("findLoandisplayById/{loandisplayid}")
+	public ModelAndView findLoandisplayById(@PathVariable Integer loandisplayid) {
+		Loandisplay loandisplay=investServiceImpl.findLoandisplayById(loandisplayid);
+		ModelAndView mav=new ModelAndView("invest/info");
+		mav.addObject("loandisplay", loandisplay);
+		return mav;
+	}
+	
+	@RequestMapping("purchase")
+	public ModelAndView purchase(Integer loandisplayid,Double investamount) {
+		System.out.println(loandisplayid+" "+investamount);
+		return null;
 	}
 
 }
