dcc52dd53c21ca73f8132b6583d92fd83aac5cca
dhb <1837652790@qq.com>
2019-08-28 15:59:20 +0800

    xiugai

--- src/main/java/com/woniu/service/TimeTest.java
+++ src/main/java/com/woniu/service/TimeTest.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TimeTest {
-	@Scheduled(cron="0/3 * * * * ?")
-	public void sendSms() {
-		System.out.println("给你发短信");
-	}
+	/*
+	 * @Scheduled(cron="0/3 * * * * ?") public void sendSms() {
+	 * System.out.println("给你发短信"); }
+	 */
 }
